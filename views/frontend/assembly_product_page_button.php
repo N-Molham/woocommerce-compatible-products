4fb6bb101672cd6fd0d9f5a584afb35c79f8a73c
Nabeel <Nabeel@LAPTOP-MAVJV76R>
2016-07-12 21:57:29 +0200

    Product page assembly button frontend fix

--- views/frontend/assembly_product_page_button.php
+++ views/frontend/assembly_product_page_button.php
@@ -6,10 +6,4 @@
  * Time: 7:43 PM
  */
 ?>
-<div class="add-links-wrap assembly-link-wrap">
-	<div class="add-links assembly-link clearfix">
-		<a rel="nofollow" href="<?php echo esc_attr( $button_link ); ?>" class="button add_to_cart_read_more">
-			<?php echo esc_html( $button_label ); ?>
-		</a>
-	</div>
-</div>
+<a rel="nofollow" href="<?php echo esc_attr( $button_link ); ?>" class="button add_to_cart_read_more"><?php echo esc_html( $button_label ); ?></a>
